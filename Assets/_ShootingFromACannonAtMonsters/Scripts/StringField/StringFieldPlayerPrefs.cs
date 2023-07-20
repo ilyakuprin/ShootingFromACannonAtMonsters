9d0142a7ed011e017ef12cb9e27b669991a86f71
ilyakuprin <ilia6478@gmail.com>
2023-07-20 16:12:05 +0300

    add icon and cursor

--- Assets/_ShootingFromACannonAtMonsters/Scripts/StringField/StringFieldPlayerPrefs.cs
+++ Assets/_ShootingFromACannonAtMonsters/Scripts/StringField/StringFieldPlayerPrefs.cs
@@ -3,9 +3,7 @@ namespace ShootingFromACannonAtMonsters
     public class StringFieldPlayerPrefs
     {
         private readonly string _totalSumRecord = "TotalSumRecord";
-        private readonly string _name = "Name";
 
         public string TotalSumRecord { get => _totalSumRecord; }
-        public string Name { get => _name; }
     }
 }
